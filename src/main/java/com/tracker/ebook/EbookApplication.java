9f126784d0e5f0637819a75ece6f61a5a3009e1f
klavoie1 <kenlavoie19@gmail.com>
2026-03-31 22:44:57 -0400

    added annotation for Mongo

--- src/main/java/com/tracker/ebook/EbookApplication.java
+++ src/main/java/com/tracker/ebook/EbookApplication.java
@@ -1,9 +1,12 @@
 package com.tracker.ebook;
 
+import com.tracker.ebook.repositories.EbookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class EbookApplication {
 
 	public static void main(String[] args) {
