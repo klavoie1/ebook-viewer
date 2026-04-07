0077cd4153144165c0ba1ae0de3a9f1ae60f65ec
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 16:50:46 -0400

    Removed unused imports; Fixed wrong method

--- src/main/java/com/tracker/ebook/repositories/EbookRepository.java
+++ src/main/java/com/tracker/ebook/repositories/EbookRepository.java
@@ -1,13 +1,13 @@
 package com.tracker.ebook.repositories;
 
 import com.tracker.ebook.entities.Ebook;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface EbookRepository extends MongoRepository<Ebook, String> {
+public interface EbookRepository extends MongoRepository<Ebook, ObjectId> {
 
-    Optional<Ebook> findAllEbooks();
+    Ebook findByTitle(String title);
+
 }
