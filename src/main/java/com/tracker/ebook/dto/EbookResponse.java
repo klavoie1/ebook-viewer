bf5a9ac0999d42e9656be94d34664cd32a5f90a4
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 18:33:38 -0400

    Implemented a Response for Ebook

--- src/main/java/com/tracker/ebook/dto/EbookResponse.java
+++ src/main/java/com/tracker/ebook/dto/EbookResponse.java
@@ -1,4 +1,29 @@
 package com.tracker.ebook.dto;
 
+import com.tracker.ebook.entities.Metadata;
+
+import java.util.List;
+
 public class EbookResponse {
+
+    private String id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private String publishedDate;
+
+    private List<String> genre;
+
+    private String summary;
+
+    private List<String> characters;
+
+    private Metadata metadata;
+
+    public EbookResponse(boolean b, Object o) {
+    }
 }
