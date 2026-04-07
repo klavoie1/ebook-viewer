0f26dafc2166ac0383c3f6a798c2813886139b81
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 16:52:19 -0400

    Replaced ArrayList w/ List; Updated deleteEbookById to catch exceptions

--- src/main/java/com/tracker/ebook/services/EbookServiceImpl.java
+++ src/main/java/com/tracker/ebook/services/EbookServiceImpl.java
@@ -2,10 +2,11 @@ package com.tracker.ebook.services;
 
 import com.tracker.ebook.entities.Ebook;
 import com.tracker.ebook.repositories.EbookRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,12 +16,12 @@ public class EbookServiceImpl implements EbookService {
     EbookRepository ebookRepository;
 
     @Override
-    public ArrayList<Ebook> findAllEbooks() {
-        return (ArrayList<Ebook>) ebookRepository.findAll();
+    public List<Ebook> findAllEbooks() {
+        return ebookRepository.findAll();
     }
 
     @Override
-    public Ebook findEbookById(String id) {
+    public Ebook findEbookById(ObjectId id) {
         Optional<Ebook> ebook = ebookRepository.findById(id);
         return ebook.orElse(null);
     }
@@ -31,7 +32,11 @@ public class EbookServiceImpl implements EbookService {
     }
 
     @Override
-    public void deleteAllEbooks() {
-        ebookRepository.deleteAll();
+    public Boolean deleteEbookById(ObjectId id) {
+        Ebook selectedEbook = ebookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ebook not found"));
+        ebookRepository.delete(selectedEbook);
+        return true;
     }
+
 }
