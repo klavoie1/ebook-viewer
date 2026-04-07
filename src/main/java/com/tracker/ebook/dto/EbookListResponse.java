f72da37679071aaea8fa3615cec175f6ce80b071
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 18:33:28 -0400

    Implemented a Response for Ebook List

--- src/main/java/com/tracker/ebook/dto/EbookListResponse.java
+++ src/main/java/com/tracker/ebook/dto/EbookListResponse.java
@@ -1,4 +1,27 @@
 package com.tracker.ebook.dto;
 
+import com.tracker.ebook.entities.Ebook;
+
+import java.util.List;
+
 public class EbookListResponse {
+
+    private boolean success;
+
+    private List<Ebook> ebookList;
+
+    public EbookListResponse(boolean success, List<Ebook> ebookList) {
+        this.success = success;
+        this.ebookList = ebookList;
+    }
+
+    public EbookListResponse() {}
+
+    public boolean isSuccess() { return success; }
+
+    public List<Ebook> getEbookList() { return ebookList; }
+
+    public void setSuccess(boolean success) { this.success = success; }
+
+    public void setEbookList(List<Ebook> ebookList) { this.ebookList = ebookList; }
 }
