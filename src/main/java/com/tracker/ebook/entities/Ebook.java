3fe7894743e1fe06cef7cf2abaf31237d5e745e1
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 19:15:07 -0400

    Implemented builder to Entity

--- src/main/java/com/tracker/ebook/entities/Ebook.java
+++ src/main/java/com/tracker/ebook/entities/Ebook.java
@@ -58,6 +58,17 @@ public class Ebook {
 
     public Ebook() {}
 
+    public Ebook(Builder builder) {
+        this.title = builder.title;
+        this.author = builder.author;
+        this.isbn = builder.isbn;
+        this.publishedDate = builder.publishedDate;
+        this.genre = builder.genre;
+        this.summary = builder.summary;
+        this.characters = builder.characters;
+        this.metadata = builder.metadata;
+    }
+
     public ObjectId getid() { return _id; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
@@ -84,5 +95,60 @@ public class Ebook {
                 "Ebook[id=%s, title=%s, author=%s, isbn=%s, publishedDate=%s]",
                 _id, title, author, isbn, publishedDate
         );
+    }
+
+    public static class Builder {
+        private String title;
+        private String author;
+        private String isbn;
+        private String publishedDate;
+        private List<String> genre;
+        private String summary;
+        private List<String> characters;
+        private Metadata metadata;
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder isbn(String isbn) {
+            this.isbn = isbn;
+            return this;
+        }
+
+        public Builder publishedDate(String publishedDate) {
+            this.publishedDate = publishedDate;
+            return this;
+        }
+
+        public Builder genre(List<String> genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public Builder summary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public Builder characters(List<String> characters) {
+            this.characters = characters;
+            return this;
+        }
+
+        public Builder metadata(Metadata metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public Ebook build() {
+            return new Ebook(this);
+        }
     }
 }
