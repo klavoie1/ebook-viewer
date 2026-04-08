737ceeadad8c23a7a2f8866677eeb258e0437ec5
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:22:59 -0400

    Added Repository annotation; Fixed wrong Character import call

--- src/main/java/com/tracker/ebook/repositories/CharacterRepository.java
+++ src/main/java/com/tracker/ebook/repositories/CharacterRepository.java
@@ -2,6 +2,9 @@ package com.tracker.ebook.repositories;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.tracker.ebook.entities.Character;
 
+@Repository
 public interface CharacterRepository extends MongoRepository<Character, ObjectId> {
 }
