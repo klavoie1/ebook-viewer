65b895ff12aaa3a07b6b129741b9c0a77ea29318
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:21:48 -0400

    Implemented POST, GET and DELETE

--- src/main/java/com/tracker/ebook/controllers/CharacterController.java
+++ src/main/java/com/tracker/ebook/controllers/CharacterController.java
@@ -1,4 +1,59 @@
 package com.tracker.ebook.controllers;
 
+import com.tracker.ebook.dto.CharacterListResponse;
+import com.tracker.ebook.dto.CharacterResponse;
+import com.tracker.ebook.services.CharacterServiceImpl;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ebookviewer/character")
 public class CharacterController {
+
+    @Autowired
+    CharacterServiceImpl characterServiceImpl;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addCharacter(@RequestBody CharacterResponse characterResponse) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(characterServiceImpl.addCharacter(characterResponse));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/allcharacters")
+    public ResponseEntity<?> getAllCharacters() {
+        try {
+            return ResponseEntity.ok(characterServiceImpl.findAllCharacters());
+        } catch (Exception e) {
+            return ResponseEntity.ok(new CharacterListResponse(false, null));
+        }
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<?> getCharacterById(@PathVariable ObjectId id) {
+        try {
+            return ResponseEntity.ok(characterServiceImpl.findCharacterById(id));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new CharacterListResponse(false, null));
+        }
+    }
+    
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deleteCharacter(@PathVariable ObjectId id) {
+        try {
+            boolean removed = characterServiceImpl.deleteCharacter(id);
+            if (removed) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/tracker/ebook/services/CharacterServiceImpl.java
+++ src/main/java/com/tracker/ebook/services/CharacterServiceImpl.java
@@ -1,4 +1,62 @@
 package com.tracker.ebook.services;
 
-public class CharacterServiceImpl {
+import com.tracker.ebook.dto.CharacterListResponse;
+import com.tracker.ebook.dto.CharacterResponse;
+import com.tracker.ebook.repositories.CharacterRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.tracker.ebook.entities.Character;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CharacterServiceImpl implements CharacterService {
+
+    @Autowired
+    CharacterRepository characterRepository;
+
+    /**
+     * @return Return a full list of all characters in database
+     */
+    @Override
+    public List<Character> findAllCharacters() { return new CharacterListResponse(true, characterRepository.findAll()).getCharactersList(); }
+
+    /**
+     * @param id
+     * @return Returns a character based on id entered. If no character exists, return null
+     */
+    @Override
+    public Character findCharacterById(ObjectId id) {
+        Optional<Character> character = characterRepository.findById(id);
+        return character.orElse(null);
+    }
+
+    /**
+     * @param characterResponse
+     * @return Add a character with the constraints that character entity describes.
+     */
+    @Override
+    public Character addCharacter(CharacterResponse characterResponse) {
+        Character newCharacter = new Character.Builder()
+                .name(characterResponse.getName())
+                .description(characterResponse.getDescription())
+                .imagePath(characterResponse.getImagePath())
+                .build();
+
+        return characterRepository.save(newCharacter);
+    }
+
+    /**
+     * @param id
+     * @return Deletes character based on Id.
+     */
+    @Override
+    public Boolean deleteCharacter(ObjectId id) {
+        Character character = characterRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Character not found"));
+        characterRepository.delete(character);
+        return true;
+    }
 }
