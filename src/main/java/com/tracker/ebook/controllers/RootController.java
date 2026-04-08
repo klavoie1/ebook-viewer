f8568a1adab29424cea558e1bfd296f202bfa091
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 23:06:41 -0400

    Implemented a simple Health check API

--- src/main/java/com/tracker/ebook/controllers/RootController.java
+++ src/main/java/com/tracker/ebook/controllers/RootController.java
@@ -1,4 +1,13 @@
 package com.tracker.ebook.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class RootController {
+
+    @GetMapping
+    public String healthCheck() { return "OK"; }
 }
