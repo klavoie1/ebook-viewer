46776838267a1aebced6be145b55d69c931ff802
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:22:26 -0400

    Response for Character List

--- src/main/java/com/tracker/ebook/dto/CharacterListResponse.java
+++ src/main/java/com/tracker/ebook/dto/CharacterListResponse.java
@@ -1,4 +1,25 @@
 package com.tracker.ebook.dto;
 
+import com.tracker.ebook.entities.Character;
+
+import java.util.List;
+
 public class CharacterListResponse {
+
+    private boolean success;
+
+    private List<Character> charactersList;
+
+    public CharacterListResponse(boolean success, List<Character> charactersList) {
+        this.success = success;
+        this.charactersList = charactersList;
+    }
+
+    public CharacterListResponse() {}
+
+    public boolean isSuccess() { return success; }
+    public List<Character> getCharactersList() { return charactersList; }
+
+    public void setSuccess(boolean success) { this.success = success; }
+    public void setCharactersList(List<Character> charactersList) { this.charactersList = charactersList; }
 }
