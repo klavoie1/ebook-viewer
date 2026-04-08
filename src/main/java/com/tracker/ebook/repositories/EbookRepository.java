56a81e831cb557a2d40e2cf6ad892145737bc3d7
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:24:38 -0400

    Removed unused findByTitle method

--- src/main/java/com/tracker/ebook/repositories/EbookRepository.java
+++ src/main/java/com/tracker/ebook/repositories/EbookRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EbookRepository extends MongoRepository<Ebook, ObjectId> {
-
-    Ebook findByTitle(String title);
-
 }
