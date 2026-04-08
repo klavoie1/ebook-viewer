9339d957437c816f8bd253a01a7f0ffa3ba7bd62
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:23:30 -0400

    Added response for Character Entity

--- src/main/java/com/tracker/ebook/dto/CharacterResponse.java
+++ src/main/java/com/tracker/ebook/dto/CharacterResponse.java
@@ -1,4 +1,33 @@
 package com.tracker.ebook.dto;
 
+import org.bson.types.ObjectId;
+
 public class CharacterResponse {
+
+    private ObjectId id;
+
+    private String name;
+
+    private String description;
+
+    private String imagePath;
+
+    public CharacterResponse() {}
+
+    public CharacterResponse(ObjectId id, String name, String description, String imagePath) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+
+    public ObjectId getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getImagePath() { return imagePath; }
+
+    public void setId(ObjectId id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setImagePath(String imagePath) { this.imagePath = imagePath; }
 }
