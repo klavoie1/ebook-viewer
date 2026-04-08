5a9aeaefae21df4ad4b67220b86d092d0f5b5411
klavoie1 <kenlavoie19@gmail.com>
2026-04-07 22:23:49 -0400

    Added service methods for CRUD

--- src/main/java/com/tracker/ebook/services/CharacterService.java
+++ src/main/java/com/tracker/ebook/services/CharacterService.java
@@ -1,4 +1,18 @@
 package com.tracker.ebook.services;
 
+import com.tracker.ebook.dto.CharacterResponse;
+import org.bson.types.ObjectId;
+import com.tracker.ebook.entities.Character;
+
+import java.util.List;
+
 public interface CharacterService {
+
+    List<Character> findAllCharacters();
+
+    Character findCharacterById(ObjectId id);
+
+    Character addCharacter(CharacterResponse characterResponse);
+
+    Boolean deleteCharacter(ObjectId id);
 }
