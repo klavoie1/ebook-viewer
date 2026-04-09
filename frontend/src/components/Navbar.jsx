9d23c0822c9f5e4d25cd6f0caf65fbe6495ba154
klavoie1 <kenlavoie19@gmail.com>
2026-04-09 01:06:34 -0400

    Changed navbar layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,23 +15,27 @@ export default function Navbar() {
 
     return (
         <nav className="bg-surface-a20 text-white p-4 sticky top-0 z-50 shadow-md border-b-2 border-surface-a50">
-            <div className="max-w-6xl mx-auto flex justify-between items-center">
-                <Link to="/" className="text-2xl font-bold">Ebook Tracker</Link>
+            <div className="flex flex-row justify-between items-center">
 
-                <form onSubmit={handleSearch} className="flex-1 mx-8 border border-light-a0 rounded-lg hover:border-primary-a0">
-                    <input
-                        type="text"
-                        placeholder="Search Ebooks, Characters..."
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        className="w-full px-4 py-2 rounded-md text-surface-a50 focus:outline-none focus:ring-2 focus:ring-primary-a0"
-                    />
-                </form>
+                <Link to="/" className="text-2xl font-bold shrink-0">Ebook Tracker</Link>
 
-                <div className="space-x-6">
-                    <Link to="/" className="hover:underline hover:animate-pulse">Home</Link>
-                    <Link to="/ebooks" className="hover:underline hover:animate-pulse">Ebooks</Link>
+                <div className="flex items-center space-x-8">
+                    <form onSubmit={handleSearch} className="w-96 border border-light-a0 rounded-lg hover:border-primary-a0">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            className="w-full px-4 py-2 rounded-md text-surface-a50 focus:outline-none focus:ring-2 focus:ring-primary-a0"
+                        />
+                    </form>
+
+                    <div className="space-x-6 shrink-0">
+                        <Link to="/" className="hover:underline hover:animate-pulse hover:text-primary-a40">Home</Link>
+                        <Link to="/ebooks" className="hover:underline hover:animate-pulse hover:text-primary-a40">Ebooks</Link>
+                    </div>
                 </div>
+
             </div>
         </nav>
     );
