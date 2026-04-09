ab8702dfe3c5e435ff377debd87485b27331315e
klavoie1 <kenlavoie19@gmail.com>
2026-04-08 23:33:02 -0400

    Created new method for fetching image from local computer

--- src/main/java/com/tracker/ebook/controllers/EbookController.java
+++ src/main/java/com/tracker/ebook/controllers/EbookController.java
@@ -5,9 +5,15 @@ import com.tracker.ebook.dto.EbookResponse;
 import com.tracker.ebook.services.EbookServiceImpl;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/ebookviewer/ebook")
@@ -63,6 +69,25 @@ public class EbookController {
             return ResponseEntity.ok(ebookServiceImpl.updateEbook(id, ebookResponse));
         }  catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/image/{filename}")
+    public ResponseEntity<Resource> getCoverImage(@PathVariable String filename) {
+        try {
+            Path imagePath = Paths.get("/home/kenneth/Documents/JavaCoding/EbookViewerCoverImages").resolve(filename);
+
+            Resource resource = new FileSystemResource(imagePath);
+
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
     }
 
